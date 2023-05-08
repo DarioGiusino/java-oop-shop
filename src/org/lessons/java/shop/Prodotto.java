86d3d2ebe3ea1b6e878cda495f587575b500a073
DarioGiusino <dario.giusino@gmail.com>
2023-05-08 15:28:21 +0200

    defined methods in Prodotto class

--- src/org/lessons/java/shop/Prodotto.java
+++ src/org/lessons/java/shop/Prodotto.java
@@ -1,5 +1,7 @@
 package org.lessons.java.shop;
 
+import java.util.Random;
+
 public class Prodotto {
 	
 	private int id;
@@ -7,4 +9,54 @@ public class Prodotto {
 	private String description;
 	private double price;
 	private int vat;
+
+	Random rnd = new Random();
+	
+	public Prodotto(String name, String description, double price, int vat) {
+		id = rnd.nextInt();
+	}
+	
+	public int getId() {
+		return id;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	
+	public double getPrice() {
+		return price;
+	}
+	public void setPrice(double price) {
+		if(price <= 0) return;
+		
+		this.price = price;
+	}
+	
+	public int getVat() {
+		return vat;
+	}
+	public void setVat(int vat) {
+		if(vat <= 0) return;
+		
+		this.vat = vat;
+	}
+	
+	public double getCommercialPrice() {
+		return price += (price * vat) / 100;
+	}
+	
+	public String getCommercialName() {
+		return "[codice prodotto: " + id + "]" + " " + name;
+	}
 }
